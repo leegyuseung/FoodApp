e4a51415eb1fff871b82330ce7a7e92a7d8c5f83
lee gyuseung <gyus0421@gmail.com>
2024-01-07 18:07:52 +0900

    React Native FoodApp : 화면 간 데이터전달을 위해 라우트 매개변수로 작업

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
+
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+// screen으로 등록된 화면은 route props 사용
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 }
